735c89505a8049c200e34216807c94afbf4843d3
omarbramirez <omar.ramirez94@hotmail.es>
2024-04-06 22:21:15 -0600

    video margin fixed

--- src/pages/Ofidias.jsx
+++ src/pages/Ofidias.jsx
@@ -153,11 +153,14 @@ return(
         }
       </ul>
     </div>
+    <div className="wrapped" style={{display: 'flex'}}>
+
     <video controls autoPlay muted>
       <source src={Video} type="video/mp4" />
       {/* <source src="movie.ogg" type="video/ogg" /> */}
       Your browser does not support the video tag.
     </video>
+    </div>
 
     </div>
     </>
