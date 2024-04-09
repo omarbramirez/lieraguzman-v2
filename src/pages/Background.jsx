568597f4d61b13fd513cb371fed545babbdb46da
omarbramirez <omar.ramirez94@hotmail.es>
2024-04-08 23:26:56 -0600

    background section completed

--- src/pages/Background.jsx
+++ src/pages/Background.jsx
@@ -1,7 +1,45 @@
 function Background (){
 return(
     <>
-    <h1>BACKGROUND</h1>
+ <div className="content content-background" style={{ width: '70%' }}>
+            <div className="header" style={{ marginTop: '5%' }}>
+                <h1 className="title-light">Perfil</h1>
+                <p>Poeta, traductora, docente y lexicógrafa. Directora del proyecto <b>Mujeres al oído</b>, audios de escritoras ecuatorianas contemporáneas. Trabaja para la <b>Academia Ecuatoriana de la Lengua</b> y trabajó para la <b>Academia Mexicana de la Lengua</b> (2016-2020). Ha impartido clases de lexicografía, semántica y teorías lingüísticas en la <b>Benemérita Universidad Autónoma de Puebla en la Licenciatura de Lingüística y Literatura Hispánica</b> y clases de lingüística para psicoanalistas en la <b>Escuela de la Letra Psicoanalítica</b>. Tradujo al español <b>Une histoire de Bleu</b> de Jean-Michel Maulpoix (ganador del Premio Goncourt de Poesía) publicado por el <b>Círculo de Poesía</b> y el <b>Mercure de France</b> (2022). Ha sido curadora del <b>Salón de Poesía</b> de la <b>FILQUITO 2020 y 2021</b>.</p>
+            </div>
+
+            <section>
+                <div className="list">
+                    <h1 className="list-title title-light">Formación</h1>
+                    <br />
+                    <p className="list-element list-element-light">Licenciada en Lingüística y Literatura Hispánica por la Benemérita Universidad Autónoma de Puebla.</p>
+                    <p className="list-element list-element-light">Maestría en Lingüística Hispánica por la Universidad Nacional Autónoma de México.</p>
+                    <p className="list-element list-element-light">Máster en Lexicografía Hispánica por la Real Academia de la Lengua Española y la Universidad de León.</p>
+                </div>
+                <div className="list">
+                    <h1 className="list-title title-light">Publicaciones</h1>
+                    <br />
+                    <p className="list-element list-element-light">2010:  &quot;Efusiva penitente &quot;, libro de poemas publicado por la Casa de la Cultura Ecuatoriana, colección Ignacio Burbano.</p>
+                    <p className="list-element list-element-light">2019:  &quot;Ofidias &quot;, libro de poemas publicado por el Instituto Municipal de Arte y Cultura de Tijuana.</p>
+                    <p className="list-element list-element-light">2022:  &quot;Animalemas &quot;, libro interactivo de poemas para niños publicado por editorial Osa de anteojos y el Ministerio de Cultura y Patrimonio del Ecuador.</p>
+                </div>
+                <div className="list">
+                    <h1 className="list-title title-light">Reconocimientos obtenidos</h1>
+                    <br />
+                    <p className="list-element list-element-light">Noviembre de 2009: Ganadora del X Premio Filosofía y Letras de la Benemérita Universidad Autónoma de Puebla con su poemario  &quot;Constelada &quot;.</p>
+                    <p className="list-element list-element-light">Octubre de 2010: Ganadora del XI Premio Filosofía y Letras de la Benemérita Universidad Autónoma de Puebla con el ensayo  &quot;Por puro gusto lexicográfico: un acercamiento a la labor de Joaquín García Icazbalceta en el Vocabulario de Mexicanismos &quot;.</p>
+                    <p className="list-element list-element-light">Mayo de 2012: Examen profesional (Tesis: Los animales en tradición lexicográfica de la lengua española (del siglo XVII al XXI)) aprobado por Cum Laude.</p>
+                    {/* Otros reconocimientos */}
+                </div>
+                <div className="list">
+                    <h1 className="list-title title-light">Idiomas</h1>
+                    <br />
+                    <p className="list-element list-element-light">Latín.</p>
+                    <p className="list-element list-element-light">Inglés.</p>
+                    <p className="list-element list-element-light">Francés.</p>
+                    <p className="list-element list-element-light">Ruso.</p>
+                </div>
+            </section>
+        </div>
     </>
 )
 }
